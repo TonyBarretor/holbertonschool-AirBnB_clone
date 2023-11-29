db8d60cf9ceb5eead06a630d7fd7fcdef2d1d009
TonyBarretor <tony.barretor@gmail.com>
2023-11-28 21:21:35 -0800

    fix: added 2 unittest to test_amenity.py, test_review.py and engine/test_file_storage.py

--- tests/test_models/engine/test_file_storage.py
+++ tests/test_models/engine/test_file_storage.py
@@ -15,15 +15,26 @@ class TestFileStorage(unittest.TestCase):
     def test_save_method(self):
         my_storage = FileStorage()
         my_storage.save()
-        # Agrega pruebas específicas para el método save si es necesario
+        # Add specific tests for the save method if needed
 
     def test_new_method(self):
         my_storage = FileStorage()
-        # Agrega pruebas específicas para el método new si es necesario
+        # Add specific tests for the new method if needed
 
     def test_reload_method(self):
         my_storage = FileStorage()
-        # Agrega pruebas específicas para el método reload si es necesario
+        # Add specific tests for the reload method if needed
+
+    def test_custom_functionality(self):
+        my_storage = FileStorage()
+        # Test custom functionality, for example, a specific method.
+        my_storage.set_custom_config("Custom Value")
+        self.assertEqual(my_storage.get_custom_config(), "Custom Value")
+
+    def test_edge_case(self):
+        # Test an edge case, for example, handling an empty data input.
+        my_storage = FileStorage(data="")
+        self.assertEqual(my_storage.data, "")
 
 
 if __name__ == '__main__':
