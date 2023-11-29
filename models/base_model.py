172748aa610c75d0d77bd480a2744d6285c88cf1
TonyBarretor <tony.barretor@gmail.com>
2023-11-28 17:38:28 -0800

    feat:added dir model and base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+BaseModel module for AirBnB clone project.
+"""
+
+
+class BaseModel:
+    """
+    BaseModel class for representing the base model.
+    """
+    def _init_(self):
+        """Initialize BaseModel instance."""
+        pass
+
+
+if _name_ == '_main_':
+    my_model = BaseModel()
+    print(my_model)
