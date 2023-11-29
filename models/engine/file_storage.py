04b44e21d2ce63152a99ad7ac97ee16e94e09b4b
TonyBarretor <tony.barretor@gmail.com>
2023-11-28 18:28:26 -0800

    feat: added models/review.py and models/engine/file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+FileStorage module for AirBnB clone project.
+"""
+
+
+class FileStorage:
+    """
+    FileStorage class for handling the serialization and deserialization
+    of instances to and from JSON format.
+    """
+    def _init_(self):
+        """Initialize FileStorage instance."""
+        pass
+
+
+if _name_ == '_main_':
+    my_storage = FileStorage()
+    print(my_storage)
